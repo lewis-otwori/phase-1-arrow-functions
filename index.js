3de69e1b09b83c68435b7aebd93439d8ff7a3575
lewis-otwori <lewisotwori@gmail.com>
2023-03-15 16:24:19 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+function divide() {
+    return 2000 / 100;
+  }
+
+const square = (squareArrow) => squareArrow * 2
+
+square(2, 2)
+
+const add = (addArrow, arrowAdd) => addArrow + arrowAdd;
+add(5, 2)
